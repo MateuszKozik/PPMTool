963d62e668d842c162b89cd3bfd141b382e9d497
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-04-20 13:31:42 +0200

    Create project service

---
+++ ppmtool/src/main/java/com/kozik/ppmtool/services/ProjectService.java
@@ -0,0 +1,18 @@
+package com.kozik.ppmtool.services;
+
+import com.kozik.ppmtool.domain.Project;
+import com.kozik.ppmtool.repositories.ProjectRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+        return projectRepository.save(project);
+    }
+}
