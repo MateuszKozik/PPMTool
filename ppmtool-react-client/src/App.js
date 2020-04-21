13e5a6fe0f32e39fd1089b6ec56a1d68921c9c2f
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-04-21 13:51:17 +0200

    Import router

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
 import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
         <Header />
         <Dashboard />
-    </div>
+      </div>
+    </Router>
+
   );
 }
 
