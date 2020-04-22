f751acfc5473136fc07ea85daeb9286305408df7
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-04-22 21:41:25 +0200

    Create update form

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import './App.css';
-import Dashboard from './components/Dashboard';
-import Header from './components/Layout/Header';
+import React from "react";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import AddProject from './components/Project/AddProject';
+import AddProject from "./components/Project/AddProject";
 import { Provider } from "react-redux";
 import store from "./store";
+import UpdateProject from "./components/Project/UpdateProject";
 
 function App() {
-  return (
-    <Provider store={store}>
-      <Router>
-        <div className="App">
-          <Header />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/addProject" component={AddProject} />
-        </div>
-      </Router>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <Router>
+                <div className="App">
+                    <Header />
+                    <Route path="/dashboard" component={Dashboard} />
+                    <Route path="/addProject" component={AddProject} />
+                    <Route
+                        path="/updateProject/:id"
+                        component={UpdateProject}
+                    />
+                </div>
+            </Router>
+        </Provider>
+    );
 }
 
 export default App;
