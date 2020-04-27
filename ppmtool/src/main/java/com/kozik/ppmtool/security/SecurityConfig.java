46f38944b222c5f9c6dbebebabf4d918837338a8
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-04-27 14:16:27 +0200

    Implement JWT filter into security config

--- ppmtool/src/main/java/com/kozik/ppmtool/security/SecurityConfig.java
+++ ppmtool/src/main/java/com/kozik/ppmtool/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static com.kozik.ppmtool.security.SecurityConstants.SIGN_UP_URLS;
 
@@ -28,6 +29,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private CustomUserDetailService customUserDetailService;
 
+    @Bean
+    public JwtAuthenticationFilter jtwAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
+
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
@@ -37,6 +43,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
                 .antMatchers("/", "**/*.png", "**/*.gid", "**/*.svg", "**/*.jpg", "**/*.html", "**/*.css", "**/*.js")
                 .permitAll().antMatchers(SIGN_UP_URLS).permitAll().anyRequest().authenticated();
+
+        http.addFilterBefore(jtwAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
