e279fd86dd9c1996c9a01ec1f6d53266991bf12b
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-04-27 08:55:00 +0200

    Configure authentication manager

--- ppmtool/src/main/java/com/kozik/ppmtool/security/SecurityConfig.java
+++ ppmtool/src/main/java/com/kozik/ppmtool/security/SecurityConfig.java
@@ -1,14 +1,21 @@
 package com.kozik.ppmtool.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import static com.kozik.ppmtool.security.SecurityConstants.SIGN_UP_URLS;
+
+import com.kozik.ppmtool.services.CustomUserDetailService;
 
 @Configuration
 @EnableWebSecurity
@@ -18,12 +25,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private JwtAuthenticationEntryPoint unauthorizedHandler;
 
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
                 .antMatchers("/", "**/*.png", "**/*.gid", "**/*.svg", "**/*.jpg", "**/*.html", "**/*.css", "**/*.js")
                 .permitAll().antMatchers(SIGN_UP_URLS).permitAll().anyRequest().authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(customUserDetailService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 
 }
