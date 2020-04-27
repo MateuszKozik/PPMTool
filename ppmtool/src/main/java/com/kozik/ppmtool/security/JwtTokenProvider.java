093ca7648c6dbae82fe13460d5d534aaa0473fd6
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-04-27 14:09:27 +0200

    Create JWT authentication filter

--- ppmtool/src/main/java/com/kozik/ppmtool/security/JwtTokenProvider.java
+++ ppmtool/src/main/java/com/kozik/ppmtool/security/JwtTokenProvider.java
@@ -9,8 +9,13 @@ import com.kozik.ppmtool.domain.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 
 import static com.kozik.ppmtool.security.SecurityConstants.EXPIRATION_TIME;
 import static com.kozik.ppmtool.security.SecurityConstants.SECRET;
@@ -33,5 +38,29 @@ public class JwtTokenProvider {
 
         return Jwts.builder().setSubject(userId).setClaims(claims).setIssuedAt(now).setExpiration(expiryDate)
                 .signWith(SignatureAlgorithm.HS512, SECRET).compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+            return true;
+        } catch (SignatureException ex) {
+            System.out.println("Invalid JWT Signature");
+        } catch (MalformedJwtException ex) {
+            System.out.println("Invalid JWT Token");
+        } catch (ExpiredJwtException ex) {
+            System.out.println("Expired JWT Token");
+        } catch (UnsupportedJwtException ex) {
+            System.out.println("Unsupported JWT Token");
+        } catch (IllegalArgumentException ex) {
+            System.out.println("JWT claims string is empty");
+        }
+        return false;
+    }
+
+    public Long getUserIdFromJWT(String token) {
+        Claims claims = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+        String id = (String) claims.get("id");
+        return Long.parseLong(id);
     }
 }
